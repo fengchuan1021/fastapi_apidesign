153bca77be6c49abf75fde4df85c1f62ec60fe1b
fengchuan <fengchuanhn@gmail.com>
2022-07-19 13:52:42 +0800

    firt commit

--- modules/backend/__init__.py
+++ modules/backend/__init__.py
@@ -3,15 +3,13 @@ from .. import dependencies as praentdependencies
 from fastapi import Depends
 from typing import List,Callable,Any
 import settings
-from fastapi.encoders import jsonable_encoder
-from fastapi import HTTPException
+#from fastapi.encoders import jsonable_encoder
 from common.globalFunctions import get_token
-from common.CommonError import Common500Status,Common500OutShema,TokenException
-from starlette.responses import JSONResponse
-def check_admintoken(token: settings.UserTokenData = Depends(get_token))->Any:
+from common.CommonError import TokenException
+
+def check_admintoken(token: settings.UserTokenData = Depends(get_token))->None:
     if not token.is_admin:
-        out=jsonable_encoder(Common500OutShema(status=Common500Status.tokenerror,msg="not admin",data=""))
-        print('1111')
+        #out=jsonable_encoder(Common500OutShema(status=Common500Status.tokenerror,msg="not admin",data=""))
         raise TokenException("not admin")
 
 dependencies:List[Callable[...,Any]]=praentdependencies+[Depends(check_admintoken)]
